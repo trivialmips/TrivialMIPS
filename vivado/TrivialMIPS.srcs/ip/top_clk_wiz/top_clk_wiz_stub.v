436edb279bf6b683e0fabd4ea48c453fb519a5b6
Harry Chen <i@harrychen.xyz>
2018-07-08 20:22:56 +0800

    Add generated clocking IP
    Signed-off-by: Harry Chen <i@harrychen.xyz>

---
+++ vivado/TrivialMIPS.srcs/ip/top_clk_wiz/top_clk_wiz_stub.v
@@ -0,0 +1,25 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
+// Date        : Sun Jul  8 20:17:52 2018
+// Host        : Harry-XPS running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub {c:/Users/Harry
+//               Chen/Documents/Workspace/TrivialMIPS/vivado/TrivialMIPS.srcs/ip/top_clk_wiz/top_clk_wiz_stub.v}
+// Design      : top_clk_wiz
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tfgg676-2L
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module top_clk_wiz(clk_out1, clk_out2, clk_out3, reset, locked, 
+  clk_in1)
+/* synthesis syn_black_box black_box_pad_pin="clk_out1,clk_out2,clk_out3,reset,locked,clk_in1" */;
+  output clk_out1;
+  output clk_out2;
+  output clk_out3;
+  input reset;
+  output locked;
+  input clk_in1;
+endmodule
---
+++ vivado/TrivialMIPS.srcs/ip/top_clk_wiz/top_clk_wiz_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
+-- Date        : Sun Jul  8 20:17:52 2018
+-- Host        : Harry-XPS running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub {c:/Users/Harry
+--               Chen/Documents/Workspace/TrivialMIPS/vivado/TrivialMIPS.srcs/ip/top_clk_wiz/top_clk_wiz_stub.vhdl}
+-- Design      : top_clk_wiz
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tfgg676-2L
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity top_clk_wiz is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_out2 : out STD_LOGIC;
+    clk_out3 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end top_clk_wiz;
+
+architecture stub of top_clk_wiz is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_out2,clk_out3,reset,locked,clk_in1";
+begin
+end;
