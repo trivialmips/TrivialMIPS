b65aca88a6c805e49ef17be2bb98736fbfed49d2
Harry Chen <i@harrychen.xyz>
2018-07-11 20:04:56 +0800

    Update simulation mem files to use the correct byte order
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- testbench/cpu/fake_inst_bus.sv
+++ testbench/cpu/fake_inst_bus.sv
@@ -15,15 +15,8 @@ begin
 	end else begin
 		Word_t data = inst_mem[inst_bus.address[11:2]];
 		Word_t data2 = inst_mem[inst_bus.address[11:2] + 1];
-		inst_bus.data_rd[31:24] = data[7:0];
-		inst_bus.data_rd[23:16] = data[15:8];
-		inst_bus.data_rd[15:8]  = data[23:16];
-		inst_bus.data_rd[7:0]   = data[31:24];
-
-		inst_bus.data_rd_2[31:24] = data2[7:0];
-		inst_bus.data_rd_2[23:16] = data2[15:8];
-		inst_bus.data_rd_2[15:8]  = data2[23:16];
-		inst_bus.data_rd_2[7:0]   = data2[31:24];
+		inst_bus.data_rd = data;
+		inst_bus.data_rd_2 = data2;
 	end
 end
 
--- testbench/testcase/extract_ans.awk
+++ testbench/testcase/extract_ans.awk
@@ -1,3 +1,3 @@
-BEGIN { cyc = 4 }
+BEGIN { cyc = 5 }
 match($1, /\[\+([0-9]*)\](.*)/, g) { cyc += g[1]; print "["cyc"]"g[2] }
 !/\[\+([0-9]*)\].*/ { print $1 }
