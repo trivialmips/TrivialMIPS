cf44a2c95fe8e6433fe7afd0d1930162a478d0f4
Harry Chen <i@harrychen.xyz>
2018-07-13 02:29:44 +0800

    Update modified bootrom IP
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- vivado/TrivialMIPS.srcs/ip/blk_mem_bootrom/blk_mem_bootrom_stub.vhdl
+++ vivado/TrivialMIPS.srcs/ip/blk_mem_bootrom/blk_mem_bootrom_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
--- Date        : Wed Jul 11 20:08:19 2018
+-- Date        : Fri Jul 13 01:23:22 2018
 -- Host        : Harry-XPS running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub {C:/Users/Harry
 --               Chen/Documents/Workspace/TrivialMIPS/vivado/TrivialMIPS.srcs/ip/blk_mem_bootrom/blk_mem_bootrom_stub.vhdl}
