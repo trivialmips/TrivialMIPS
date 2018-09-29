d75706cfc5ac9203fd71613598144da76d0996e2
Harry Chen <i@harrychen.xyz>
2018-09-29 17:36:23 +0800

    Rewrite Gitlab CI rules to automatically generate IP and cache result
    Signed-off-by: Harry Chen <i@harrychen.xyz>

---
+++ vivado/scripts/generate_all_ip.tcl
@@ -0,0 +1,19 @@
+update_compile_order -fileset sources_1
+
+# If IP cores are used
+if { [llength [get_ips]] != 0} {
+    upgrade_ip [get_ips]
+
+    foreach ip [get_ips] {
+        create_ip_run [get_ips $ip]
+    }
+
+    set ip_runs [get_runs -filter {SRCSET != sources_1 && IS_SYNTHESIS}]
+    launch_runs -quiet -jobs 2 {*}$ip_runs
+
+    foreach r $ip_runs {
+        wait_on_run $r
+    }
+}
+
+exit
