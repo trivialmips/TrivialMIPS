60069682c13ad97701ca53351008864d8acc33de
Harry Chen <i@harrychen.xyz>
2018-09-29 11:36:43 +0800

    Use general build.tcl from z4yx
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- vivado/build.tcl
+++ vivado/build.tcl
@@ -1,19 +1,17 @@
 update_compile_order -fileset sources_1
 upgrade_ip [get_ips]
 
-create_ip_run [get_ips top_clk_wiz]
-create_ip_run [get_ips blk_mem_graphics]
-create_ip_run [get_ips fifo_uart_tx]
-create_ip_run [get_ips fifo_uart_rx]
-create_ip_run [get_ips blk_mem_bootrom]
+set ip_runs {}
+set run_suffix _synth_1
+foreach ip [get_ips] {
+    create_ip_run [get_ips $ip]
+    lappend ip_runs $ip$run_suffix
+}
+launch_runs -quiet -jobs 2 {*}$ip_runs
 
-launch_runs -quiet -jobs 2 top_clk_wiz_synth_1 blk_mem_graphics_synth_1 fifo_uart_tx_synth_1 fifo_uart_rx_synth_1 blk_mem_bootrom_synth_1
-
-wait_on_run top_clk_wiz_synth_1
-wait_on_run blk_mem_graphics_synth_1
-wait_on_run fifo_uart_tx_synth_1
-wait_on_run fifo_uart_rx_synth_1
-wait_on_run blk_mem_bootrom_synth_1
+foreach r $ip_runs {
+    wait_on_run $r
+}
 
 reset_run impl_1
 reset_run synth_1
@@ -21,3 +19,4 @@ launch_runs -jobs 2 impl_1 -to_step write_bitstream
 wait_on_run impl_1
 
 exit
+
